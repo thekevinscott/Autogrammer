b8f209e923ddcf5d971c84b56033e04df1dc0830
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-06-03 06:12:04 -0400

    Tighten parseName function

--- packages/gbnf/javascript/src/rules-builder/parse-name.test.ts
+++ packages/gbnf/javascript/src/rules-builder/parse-name.test.ts
@@ -1,24 +1,48 @@
-import { describe, it, expect, } from 'vitest';
+import {
+  describe,
+  test,
+  expect,
+} from 'vitest';
 import { GrammarParseError } from '../utils/errors.js';
-import { PARSE_NAME_ERROR, parseName } from './parse-name.js';
+import {
+  GET_INVALID_CHAR_ERROR,
+  PARSE_NAME_ERROR,
+  parseName
+} from './parse-name.js';
 
 describe('parseName', () => {
-  it('should return correct position when encountering a valid name', () => {
-    const src = 'validName';
+  test.each([
+    'v',
+    'valid',
+    'valid-name',
+    'valid-name-foo',
+    'validName',
+    'validName-foo',
+  ])('should return correct position when encountering a valid name "%s"', (src) => {
     const name = parseName(src, 0);
     expect(name).toEqual(src);
   });
 
-  it('should return correct position when encountering a valid name starting at a non-zero position', () => {
-    const src = '123validName';
-    const name = parseName(src, 3);
-    expect(name).toEqual('validName');
+  test.each([
+    ['123valid', 3, 'valid'],
+    ['123valid-name', 3, 'valid-name'],
+    ['123valid _Name', 3, 'valid'],
+    ['123valid\\n_Name', 3, 'valid'],
+    ['123valid\\t_Name', 3, 'valid'],
+    ['123valid\\r_Name', 3, 'valid'],
+  ])('should return correct position when encountering a valid name "%s" starting at a non-zero position', (src, position, expectation) => {
+    const name = parseName(src.split('\\n').join('\n'), position);
+    expect(name).toEqual(expectation);
   });
 
-  it('should throw an error when encountering an invalid name', () => {
-    const src = '123';
+  test.each([
+    ['123', new GrammarParseError('123', 0, PARSE_NAME_ERROR)],
+    ['valid_name', new GrammarParseError('valid_name', 5, GET_INVALID_CHAR_ERROR('_'))],
+    ['valid123', new GrammarParseError('valid123', 5, GET_INVALID_CHAR_ERROR('1'))],
+    // ['validName', new GrammarParseError('validName', 5, GET_INVALID_CHAR_ERROR('N'))],
+  ])('should throw an error when encountering an invalid name "%s"', (src, error) => {
     expect(() => {
       parseName(src, 0);
-    }).toThrow(new GrammarParseError(src, 0, PARSE_NAME_ERROR));
+    }).toThrow(error);
   });
 });
--- packages/gbnf/javascript/src/rules-builder/parse-name.ts
+++ packages/gbnf/javascript/src/rules-builder/parse-name.ts
@@ -1,16 +1,22 @@
 import { GrammarParseError, } from "../utils/errors.js";
-import { isWordChar, } from "./is-word-char.js";
 
 export const PARSE_NAME_ERROR = 'Failed to find a valid name';
+export const GET_INVALID_CHAR_ERROR = (char: string) => `Invalid character "${char}" when parsing name, only lowercase letters and hyphens are allowed.`;
+
+const isValidChar = (char: string) => /[a-zA-Z-]/.test(char);
+const isInvalidNextChar = (char: string) => /[_0-9]/.test(char);
 
 export const parseName = (grammar: string, pos: number): string => {
   let name = '';
-  while (pos < grammar.length && (isWordChar(grammar[pos]) || grammar[pos] === '-')) {
+  while (pos < grammar.length && (isValidChar(grammar[pos]))) {
     name += grammar[pos];
     pos++;
   }
   if (!name) {
     throw new GrammarParseError(grammar, pos, PARSE_NAME_ERROR);
+  }
+  if (pos < grammar.length && isInvalidNextChar(grammar[pos])) {
+    throw new GrammarParseError(grammar, pos, GET_INVALID_CHAR_ERROR(grammar[pos]));
   }
   return name;
 };
