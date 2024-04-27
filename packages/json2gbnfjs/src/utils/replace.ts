d62c8d1d7cdb14d76674bca2dd2a0aa6e32749d2
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-04-27 10:24:18 -0400

    Remove commented code

--- packages/json2gbnfjs/src/utils/replace.ts
+++ packages/json2gbnfjs/src/utils/replace.ts
@@ -1,11 +1,4 @@
-// import Mustache from 'mustache';
 import { KEYS, } from '../constants/grammar-keys.js';
-// export const replace = (def: unknown): string => {
-//   if (typeof def !== 'string') {
-//     throw new Error(`Expected string for ${JSON.stringify(def)}`);
-//   }
-//   return Mustache.render(def, KEYS);
-// };
 
 const GBNF_KEY_REPLACEMENT_PATTERN = /{{(.*?)}}/g;
 
