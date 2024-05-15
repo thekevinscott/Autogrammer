1549fecec29c37bb2f4c7bad9daefd4965e7a156
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-05-15 08:08:55 -0400

    Fix python tests

--- packages/json2gbnf/python/json2gbnf/json2gbnf_test.py
+++ packages/json2gbnf/python/json2gbnf/json2gbnf_test.py
@@ -36,35 +36,36 @@ WHITESPACE_REPEATING_KEY = "wss"
                 },
             },
             [
+                """root ::= json2gbnf""",
                 f"""xa ::= ({WHITESPACE_KEY})?""",
                 f"""ws{COLON_KEY}ws ::= xa {COLON_KEY} xa""",
                 f"""{LEFT_BRACE_KEY}ws ::= {LEFT_BRACE_KEY} xa""",
                 f"""ws{RIGHT_BRACE_KEY} ::= xa {RIGHT_BRACE_KEY}""",
                 f"""{COMMA_KEY}ws ::= {COMMA_KEY} xa""",
-                f"""xf ::= {QUOTE_KEY} "number" {QUOTE_KEY} wscolws {NUMBER_KEY}""",
-                f"""xg ::= {QUOTE_KEY} "street_name" {QUOTE_KEY} wscolws {STRING_KEY}""",
-                f"""xh ::= {QUOTE_KEY} "Street" {QUOTE_KEY} | {QUOTE_KEY} "Avenue" {QUOTE_KEY} | {QUOTE_KEY} "Boulevard" {QUOTE_KEY}""",
-                f"""xi ::= {QUOTE_KEY} "street_type" {QUOTE_KEY} ws{COLON_KEY}ws xh""",
-                f"""xj ::= xf {COMMA_KEY}ws xg""",
-                f"""xk ::= xf {COMMA_KEY}ws xg {COMMA_KEY}ws xi""",
-                f"""xl ::= xf {COMMA_KEY}ws xi""",
-                f"""xm ::= xf {COMMA_KEY}ws xi {COMMA_KEY}ws xg""",
-                f"""xn ::= xg {COMMA_KEY}ws xf""",
-                f"""xo ::= xg {COMMA_KEY}ws xf {COMMA_KEY}ws xi""",
-                f"""xp ::= xg {COMMA_KEY}ws xi""",
-                f"""xq ::= xg {COMMA_KEY}ws xi {COMMA_KEY}ws xf""",
-                f"""xr ::= xi {COMMA_KEY}ws xf""",
-                f"""xs ::= xi {COMMA_KEY}ws xf {COMMA_KEY}ws xg""",
-                f"""xt ::= xi {COMMA_KEY}ws xg""",
-                f"""xu ::= xi {COMMA_KEY}ws xg {COMMA_KEY}ws xf""",
-                f"""json2gbnf ::= {LEFT_BRACE_KEY}ws (xf | xj | xk | xl | xm | xg | xn | xo | xp | xq | xi | xr | xs | xt | xu)? ws{RIGHT_BRACE_KEY}""",
-                """root ::= json2gbnf""",
+                f"""xb ::= {QUOTE_KEY} "number" {QUOTE_KEY} wscolws {NUMBER_KEY}""",
+                f"""xc ::= {QUOTE_KEY} "street_name" {QUOTE_KEY} wscolws {STRING_KEY}""",
+                f"""xd ::= {QUOTE_KEY} "Street" {QUOTE_KEY} | {QUOTE_KEY} "Avenue" {QUOTE_KEY} | {QUOTE_KEY} "Boulevard" {QUOTE_KEY}""",
+                f"""xe ::= {QUOTE_KEY} "street_type" {QUOTE_KEY} ws{COLON_KEY}ws xd""",
+                f"""xf ::= xb {COMMA_KEY}ws xc""",
+                f"""xg ::= xb {COMMA_KEY}ws xc {COMMA_KEY}ws xe""",
+                f"""xh ::= xb {COMMA_KEY}ws xe""",
+                f"""xi ::= xb {COMMA_KEY}ws xe {COMMA_KEY}ws xc""",
+                f"""xj ::= xc {COMMA_KEY}ws xb""",
+                f"""xk ::= xc {COMMA_KEY}ws xb {COMMA_KEY}ws xe""",
+                f"""xl ::= xc {COMMA_KEY}ws xe""",
+                f"""xm ::= xc {COMMA_KEY}ws xe {COMMA_KEY}ws xb""",
+                f"""xn ::= xe {COMMA_KEY}ws xb""",
+                f"""xo ::= xe {COMMA_KEY}ws xb {COMMA_KEY}ws xc""",
+                f"""xp ::= xe {COMMA_KEY}ws xc""",
+                f"""xq ::= xe {COMMA_KEY}ws xc {COMMA_KEY}ws xb""",
+                f"""json2gbnf ::= {LEFT_BRACE_KEY}ws (xb | xf | xg | xh | xi | xc | xj | xk | xl | xm | xe | xn | xo | xp | xq)? ws{RIGHT_BRACE_KEY}""",
             ],
         ),
     ],
 )
 def test_it_calls_json2gbnf(schema, expected):
     result = json2gbnf(schema)
+
     assert result.split("\n")[: len(expected)] == expected
 
 
@@ -81,12 +82,12 @@ def test_it_calls_json2gbnf_with_fixed_order():
 
     result = json2gbnf(schema, fixed_order=True, whitespace=0)
     expected = [
+        """root ::= json2gbnf""",
         f"""xa ::= {QUOTE_KEY} "number" {QUOTE_KEY} {COLON_KEY} {NUMBER_KEY}""",
         f"""xb ::= {QUOTE_KEY} "street_name" {QUOTE_KEY} {COLON_KEY} {STRING_KEY}""",
         f"""xc ::= {QUOTE_KEY} "Street" {QUOTE_KEY} | {QUOTE_KEY} "Avenue" {QUOTE_KEY} | {QUOTE_KEY} "Boulevard" {QUOTE_KEY}""",
         f"""xd ::= {QUOTE_KEY} "street_type" {QUOTE_KEY} {COLON_KEY} xc""",
         f"""json2gbnf ::= {LEFT_BRACE_KEY} (xa com xb com xd) {RIGHT_BRACE_KEY}""",
-        """root ::= json2gbnf""",
     ]
     assert result.split("\n")[: len(expected)] == expected
 
@@ -104,6 +105,7 @@ def test_it_calls_json2gbnf_without_whitespace():
 
     result = json2gbnf(schema, whitespace=0)
     expected = [
+        """root ::= json2gbnf""",
         f"""xa ::= {QUOTE_KEY} "number" {QUOTE_KEY} {COLON_KEY} {NUMBER_KEY}""",
         f"""xb ::= {QUOTE_KEY} "street_name" {QUOTE_KEY} {COLON_KEY} {STRING_KEY}""",
         f"""xc ::= {QUOTE_KEY} "Street" {QUOTE_KEY} | {QUOTE_KEY} "Avenue" {QUOTE_KEY} | {QUOTE_KEY} "Boulevard" {QUOTE_KEY}""",
@@ -121,6 +123,5 @@ def test_it_calls_json2gbnf_without_whitespace():
         f"""xo ::= xd {COMMA_KEY} xb""",
         f"""xp ::= xd {COMMA_KEY} xb {COMMA_KEY} xa""",
         f"""json2gbnf ::= {LEFT_BRACE_KEY} (xa | xe | xf | xg | xh | xb | xi | xj | xk | xl | xd | xm | xn | xo | xp)? {RIGHT_BRACE_KEY}""",
-        """root ::= json2gbnf""",
     ]
     assert result.split("\n")[: len(expected)] == expected
